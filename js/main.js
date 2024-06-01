2d96f9cbf3e951b5ce64fd2fbecfbdbd3fca8336
Uzakov Abdulaziz <abdujalilovsarvar001@gmail.com>
2024-06-01 15:42:36 +0500

    CSS-9-dars
    ZEROMAX-ITCenter 35-guruh

---
+++ js/main.js
@@ -0,0 +1,41 @@
+let byWrap = document.getElementsByTagName("div").namedItem("wrap");
+let byFormGroup = document.getElementsByTagName("div").namedItem("form-group");
+let bylabel1 = document.getElementsByTagName("label").namedItem("label1");
+let bylabel2 = document.getElementsByTagName("label").namedItem("label2");
+let bylabel3 = document.getElementsByTagName("label").namedItem("label3");
+let bylabel4 = document.getElementsByTagName("label").namedItem("label4");
+let bylabel5 = document.getElementsByTagName("label").namedItem("label5");
+let bylabel6 = document.getElementsByTagName("label").namedItem("label6");
+let bylabel7 = document.getElementsByTagName("label").namedItem("label7");
+let bylabel8 = document.getElementsByTagName("label").namedItem("label8");
+let bylabel9 = document.getElementsByTagName("label").namedItem("label9");
+let bylabel10 = document.getElementsByTagName("label").namedItem("label10");
+let bylabel11 = document.getElementsByTagName("label").namedItem("label11");
+let bylabel12 = document.getElementsByTagName("label").namedItem("label12");
+let bylabel13 = document.getElementsByTagName("label").namedItem("label13");
+let byInput1 = document.getElementsByTagName("input").namedItem("firstName");
+let byInput2 = document.getElementsByTagName("input").namedItem("lastName");
+
+byWrap.style.width = "1200px";
+byWrap.style.margin = "0 auto";
+
+byFormGroup.style.width = "500px";
+byFormGroup.style.margin = "0 auto";
+byFormGroup.style.boxShadow = "0 0 10px rgba(80,80,80,0.5)";
+byFormGroup.style.padding = "1rem 1.5rem";
+byFormGroup.style.boxSizing = "border-box";
+
+bylabel1.style.width = "35%";
+byInput1.style.padding = "0.5rem 1.5rem";
+bylabel1.style.boxSizing = "border-box";
+bylabel1.style.display = "inline-block";
+bylabel1.style.marginBottom = "10px"
+bylabel1.style.textAlign = "right";
+
+byInput1.style.width = "60%";
+byInput1.style.padding = "0.5rem 1.5rem";
+byInput1.style.boxSizing = "border-box";
+byInput1.style.border = "1px solid #cccccc";
+byInput1.style.borderRadius = "4px";
+byInput1.style.display = "inline-block";
+byInput1.style.marginBottom = "10px";
